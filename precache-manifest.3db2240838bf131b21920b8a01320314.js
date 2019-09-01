c46980b83fa6cd2ddea3d7900ecd99a5302e2b23
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-01 11:34:15 +0300

    Updates

---
+++ precache-manifest.3db2240838bf131b21920b8a01320314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c6d50a42356d20bb863124183e8d39",
+    "url": "/dot-game/index.html"
+  },
+  {
+    "revision": "705e025375a5eade5c32",
+    "url": "/dot-game/static/css/main.a124168b.chunk.css"
+  },
+  {
+    "revision": "32061f72b62a6eeb1fc0",
+    "url": "/dot-game/static/js/2.30f65a4d.chunk.js"
+  },
+  {
+    "revision": "705e025375a5eade5c32",
+    "url": "/dot-game/static/js/main.11972b26.chunk.js"
+  },
+  {
+    "revision": "7e6a556c5dc090864ea4",
+    "url": "/dot-game/static/js/runtime~main.79c82d89.js"
+  }
+]);
